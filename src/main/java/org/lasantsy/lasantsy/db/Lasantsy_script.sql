0c2d8fc9627b9eb30dc7bb7cbaf711a78a836555
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-07 20:18:59 +0300

    chore: database script

---
+++ src/main/java/org/lasantsy/lasantsy/db/Lasantsy_script.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS lasantsy;
+
+CREATE DATABASE lasantsy;
+
+\c lasantsy;
+
+CREATE TABLE IF not exists station (
+    id bigserial primary key,
+    name varchar(50) not null,
+    longitude varchar(50) not null,
+    latitude varchar(50) not null,
+    employee_number int not null
+);
+
+CREATE TABLE IF not exists product (
+    id bigserial primary key,
+    name varchar(50) not null,
+    price double precision not null
+);
+
+CREATE TABLE IF not exists movement (
+    id bigserial primary key,
+    stationId bigint not null references station(id),
+    productId bigint not null references product(id),
+    value double precision not null,
+    type varchar(50),
+);
+
+CREATE TABLE IF not exists evaporation_rate (
+    id bigserial primary key,
+    stationId bigint not null references station(id),
+    value double precision not null
+);
+
+CREATE TABLE IF not exists stock (
+    id bigserial primary key,
+    stationId bigint not null references station(id),
+    valeur double precision not null
+);
