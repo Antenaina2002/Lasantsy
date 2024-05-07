67e2560a544f5a0e75f95921d18d9f70a70aa416
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-08 00:29:04 +0300

    feat: implement StationService for Station management

---
+++ src/main/java/org/lasantsy/lasantsy/service/StationService.java
@@ -0,0 +1,35 @@
+package org.lasantsy.lasantsy.service;
+
+import org.lasantsy.lasantsy.models.Station;
+import org.lasantsy.lasantsy.repository.StationRepository;
+
+import java.util.List;
+
+public class StationService {
+
+    private final StationRepository stationRepository;
+
+    public StationService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
+
+    public Station saveStation(Station station) {
+        return stationRepository.save(station);
+    }
+
+    public List<Station> getAllStations() {
+        return stationRepository.findAll();
+    }
+
+    public Station getStationById(Long id) {
+        return stationRepository.findById(id);
+    }
+
+    public void deleteStation(Station station) {
+        stationRepository.delete(station);
+    }
+
+    public void deleteStationById(Long id) {
+        stationRepository.deleteById(id);
+    }
+}
