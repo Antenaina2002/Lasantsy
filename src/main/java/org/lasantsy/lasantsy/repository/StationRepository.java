1a3f8afbae76eacc9344b56cc6d7d6288c6e3958
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-08 00:06:53 +0300

    feat: create a StationRepository

---
+++ src/main/java/org/lasantsy/lasantsy/repository/StationRepository.java
@@ -0,0 +1,40 @@
+package org.lasantsy.lasantsy.repository;
+
+import org.lasantsy.lasantsy.models.Station;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class StationRepository implements GenericRepository<Station, Long> {
+
+    private final Map<Long, Station> stations = new ConcurrentHashMap<>();
+
+    @Override
+    public Station save(Station entity) {
+        entity.setId(stations.keySet().stream().mapToLong(Long::longValue).max().orElse(0L) + 1); // Generate ID
+        stations.put(entity.getId(), entity);
+        return entity;
+    }
+
+    @Override
+    public List<Station> findAll() {
+        return new ArrayList<>(stations.values());
+    }
+
+    @Override
+    public Station findById(Long id) {
+        return stations.get(id);
+    }
+
+    @Override
+    public void delete(Station entity) {
+        stations.remove(entity.getId());
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        stations.remove(id);
+    }
+}
