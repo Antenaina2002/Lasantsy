b1a77ca7a52996323b195706861ebcdd96ab9d2f
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-07 21:49:09 +0300

    feat: created entity Station

---
+++ src/main/java/org/lasantsy/lasantsy/models/Station.java
@@ -0,0 +1,21 @@
+package org.lasantsy.lasantsy.models;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@ToString(of = {"id", "name"})
+@AllArgsConstructor
+@Getter
+@Setter
+public class Station {
+    private Long id;
+    private String name;
+    private String longitude;
+    private String latitude;
+    private int employeeNumber;
+    private Instant updatedAt = Instant.now();
+
+}
