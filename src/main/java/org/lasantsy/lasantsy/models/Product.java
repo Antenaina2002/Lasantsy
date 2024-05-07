96049c80aaceef1f32d481b2ade91c0904f4fe74
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-07 22:08:49 +0300

    feat: created entity Product

---
+++ src/main/java/org/lasantsy/lasantsy/models/Product.java
@@ -0,0 +1,20 @@
+package org.lasantsy.lasantsy.models;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = "id")
+@ToString(of = {"id", "name"})
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    private Long id;
+    private String name;
+    private double price;
+    private Instant updatedAt = Instant.now();
+
+}
