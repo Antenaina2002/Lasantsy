4cda22afa03ad5b15bb3f7a9947fe87698733dac
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-08 00:35:29 +0300

    feat: implement ProductService for Product management

---
+++ src/main/java/org/lasantsy/lasantsy/service/ProductService.java
@@ -0,0 +1,36 @@
+package org.lasantsy.lasantsy.service;
+
+import org.lasantsy.lasantsy.models.Product;
+import org.lasantsy.lasantsy.repository.ProductRepository;
+import org.lasantsy.lasantsy.repository.StationRepository;
+
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
