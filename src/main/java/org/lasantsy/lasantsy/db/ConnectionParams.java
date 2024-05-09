937cde4ed3cfac3be18bbd632a556f13bedcb560
Antenaina2002 <hei.antenaina@gmail.com>
2024-05-09 15:32:28 +0300

    feat: connection parameters created

---
+++ src/main/java/org/lasantsy/lasantsy/db/ConnectionParams.java
@@ -0,0 +1,7 @@
+package org.lasantsy.lasantsy.db;
+
+public class ConnectionParams {
+    public static final String URL = "DATABASE_URL";
+    public static final String USER = "USER";
+    public static final String PASSWORD = "PASSWORD";
+}
